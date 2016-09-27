dc77465419b89c61f001f7ebe73991f548aef84c
Thomas Narro <Thomas.Narro@gmail.com>
2016-09-27 04:37:55 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
+ini_set('display_errors', 1); # Display errors on page (instead of a log file)
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+
+    <link rel='stylesheet' href='' type='text/css'>
+
+    <title>Raffle</title>
+    <?php require 'logic.php'; ?>
+
+</head>
+<body class='placeholder'>
+
+    <h1>Raffle Application: </h1>
+
+
+
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,8 @@
+# 27 September 2016
+# Raffle Example
+
+This is my interpretation of the raffle exercise. It is heavily influenced by the CSCI-E15 lecture on the subject.
+
+This exercise helped me understand how to think about arrays for my P2 project.
+
+On a subsequent version, [fill in later]
